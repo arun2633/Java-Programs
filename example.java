195500fcc975ab8406c76fb83772daea8c75df51
Arun Prasadh <arun2633@gmail.com>
2014-12-03 22:07:36 -0600

    Initial Release #1

---
+++ example.java
@@ -0,0 +1,30 @@
+/*
+This is a simple Java program.
+Call this file "Example.java".
+*/
+class toSum{
+// Your program begins with a call to main().
+public static void main(String args[]) {
+
+	System.out.println("This is a simple Java program.");
+
+	toSum(10,15);
+
+}
+
+
+public static void toSum(int a, int b){
+	//int a;
+	//int b;
+	int c ;
+
+	//a = 10; b = 15;
+
+	c = a + b;
+
+	System.out.println("The total of " + a + " and " + b + " is " + c);
+
+}
+
+
+}
